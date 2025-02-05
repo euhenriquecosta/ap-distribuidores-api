baba5dcf9394f84dd8a4727d38b336df0faac9b2
Henrique Costa <henrique.costa@flowih.com>
2025-02-05 13:15:49 -0300

    feat: add TypeScript types for FastifyRequest and enhance JWT token handling with error management

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,34 @@
 import jwt from "jsonwebtoken";
 
+interface TokenPayload {
+  USER_ID: string;
+  EMAIL: string;
+  iat?: number;
+  exp?: number;
+}
+
 export const generateToken = (USER_ID: string, EMAIL: string): string => {
-  return jwt.sign(
-    { USER_ID, EMAIL },
-    process.env.JWT_SECRET_KEY!,
-    { expiresIn: '1h' }
-  );
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY não definida no ambiente!");
+  }
+
+  return jwt.sign({ USER_ID, EMAIL }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
 };
 
-export const verifyToken = (token: string) => {
-  return jwt.verify(token, process.env.JWT_SECRET_KEY!,); // Verifica o token
+export const verifyToken = (token: string): TokenPayload => {
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY não definida no ambiente!");
+  }
+
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET_KEY) as TokenPayload;
+  } catch (error: any) {
+    if (error.name === "TokenExpiredError") {
+      throw new Error("Token expirado!");
+    } else if (error.name === "JsonWebTokenError") {
+      throw new Error("Token inválido!");
+    } else {
+      throw new Error("Erro ao verificar o token.");
+    }
+  }
 };
