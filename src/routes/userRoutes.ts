3430a4c25a0d358b54b80b8d7d119e58120c1978
Henrique Costa <henrique.costa@flowih.com>
2025-02-10 19:37:19 -0300

    fix: add route to find user by ID with authentication

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ import { FastifyInstance } from "fastify";
 
 export const userRoutes = async (fastify: FastifyInstance) => {
   fastify.get('/users', userController.listUser);
+  fastify.get('/user', { preHandler: authMiddleware }, userController.findUser)
   fastify.post('/signup', userController.createUser);
   fastify.post("/signin", userController.loginUser);
   fastify.post("user/upload/avatar", { preHandler: authMiddleware }, userController.uploadPhoto);
